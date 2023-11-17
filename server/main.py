9faedcf9f8d7fe1ba3f3dfbe5efc9d9208a28059
kalikokalikova <calico.seders@gmail.com>
2023-11-17 13:12:09 -0800

    created user model, updated database code to use mysql, added some new endpoints

--- server/main.py
+++ server/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException, Depends
+from fastapi import FastAPI, HTTPException, Depends, status
 from typing import Annotated, List
 from sqlalchemy.orm import Session
 from pydantic import BaseModel
@@ -7,6 +7,7 @@ import models
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+models.Base.metadata.create_all(bind=engine)
 
 origins = [
     "http://localhost:3000",
@@ -20,11 +21,17 @@ app.add_middleware(
     allow_headers = ['*']
 )
 
+# pydantic models
 # validates request from client
 class MoodpointBase(BaseModel):
     label: str
     energy: int
     pleasantness: int
+    user_id: int
+
+# TODO is there a pydantic email validator?
+class UserBase(BaseModel):
+    email: str
 
 class MoodpointModel(MoodpointBase):
     id: int
@@ -41,7 +48,20 @@ def get_db():
 
 db_dependency = Annotated[Session, Depends(get_db)]
 
-models.Base.metadata.create_all(bind=engine)
+@app.post('/users', status_code=status.HTTP_201_CREATED)
+async def create_user(user: UserBase, db: db_dependency):
+    db_user = models.User(**user.dict())
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+@app.get('/users/{user_id}', status_code=status.HTTP_200_OK)
+async def get_user_by_id(user_id: int, db: db_dependency):
+    user = db.query(models.User).filter(models.User.id == user_id).first()
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+    return user
 
 @app.post('/moodpoints', response_model=MoodpointModel)
 async def create_moodpoint(moodpoint: MoodpointBase, db: db_dependency):
@@ -52,6 +72,19 @@ async def create_moodpoint(moodpoint: MoodpointBase, db: db_dependency):
     return db_moodpoint
 
 @app.get('/moodpoints', response_model=List[MoodpointModel])
-async def read_moodpoints(db: db_dependency, skip: int=0, limit: int = 100):
+async def get_all_moodpoints(db: db_dependency, skip: int=0, limit: int = 100):
     moodpoints = db.query(models.Moodpoint).offset(skip).limit(limit).all()
     return moodpoints
+
+@app.get('/moodpoints/{user_id}', response_model=List[MoodpointModel])
+async def get_moodpoints_by_user(user_id: int, db: db_dependency, skip: int=0, limit: int = 100):
+    moodpoints = db.query(models.Moodpoint).filter(models.Moodpoint.user_id == user_id).offset(skip).limit(limit).all()
+    return moodpoints
+
+@app.delete('/moodpoints/{moodpoint_id}', status_code=status.HTTP_200_OK)
+async def delete_moodpoint(moodpoint_id: int, db: db_dependency):
+    db_moodpoint = db.query(models.Moodpoint).filter(models.Moodpoint.id == moodpoint_id).first()
+    if db_moodpoint is None:
+        raise HTTPException(status_code=404, detail="Moodpoint not found")
+    db.delete(db_moodpoint)
+    db.commit()
--- server/models.py
+++ server/models.py
@@ -1,10 +1,20 @@
+# This is what sqlalchemy uses to create the tables in the db
 from database import Base
 from sqlalchemy import Column, Integer, String, Boolean
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String(100), unique=True)
+
 
 class Moodpoint(Base):
     __tablename__ = 'moodpoints'
 
     id = Column(Integer, primary_key=True, index=True)
-    label = Column(String)
+    label = Column(String(256))
     energy = Column(Integer)
     pleasantness = Column(Integer)
+    user_id = Column(Integer)
