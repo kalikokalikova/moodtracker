4124053caa8e322e7ffd8091355f2c3eb433e3a4
kalikokalikova <calico.seders@gmail.com>
2023-11-20 09:34:25 -0800

    adds created_at and updated_at to db models

--- server/models.py
+++ server/models.py
@@ -1,5 +1,6 @@
 from database import Base
-from sqlalchemy import Column, Integer, String, Boolean
+from datetime import datetime
+from sqlalchemy import Boolean, Column, DateTime, Integer, String, func
 
 
 class User(Base):
@@ -7,6 +8,9 @@ class User(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String(100), unique=True)
+    password = Column(String(100))
+    updated_at = Column(DateTime, nullable=True, default=None, onupdate=datetime.now)
+    created_at = Column(DateTime, nullable=False, server_default=func.now())
 
 
 class Moodpoint(Base):
@@ -16,4 +20,6 @@ class Moodpoint(Base):
     label = Column(String(256))
     energy = Column(Integer)
     pleasantness = Column(Integer)
+    updated_at = Column(DateTime, nullable=True, default=None, onupdate=datetime.now)
+    created_at = Column(DateTime, nullable=False, server_default=func.now())
     user_id = Column(Integer)
