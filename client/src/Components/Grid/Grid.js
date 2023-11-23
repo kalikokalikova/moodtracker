989988bbd608c37af8d2fd080c48b1cfa7171303
kalikokalikova <calico.seders@gmail.com>
2023-11-22 16:18:44 -0800

    user is prompted to log in if they click a square and they're not already logged in

--- client/src/Components/Grid/Grid.js
+++ client/src/Components/Grid/Grid.js
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import GridSquare from "./GridSquare";
+import { UserContext } from "../../Hooks/UserContext";
 import { createUseStyles } from "react-jss";
 import GridData from "./GridData";
 import { Button, useDisclosure } from "@chakra-ui/react";
-import { useToast } from '@chakra-ui/react'
+import { useToast } from "@chakra-ui/react";
 import {
   AlertDialog,
   AlertDialogBody,
@@ -16,31 +17,36 @@ import api from "../../api";
 
 const styles = createUseStyles({
   squareContainer: {
-    display: 'grid',  // Set display property to 'grid'
-    gridTemplateColumns: 'repeat(6, 1fr)', // You can adjust the number of columns as needed
-    gap: '8px', // Adjust the gap between squares
+    display: "grid", // Set display property to 'grid'
+    gridTemplateColumns: "repeat(6, 1fr)", // You can adjust the number of columns as needed
+    gap: "8px", // Adjust the gap between squares
   },
 });
 
 function Grid() {
-  const [ moodpointForm, setMoodpointForm ] = useState({
-    "energy": 0,
-    "pleasantness": 0,
-    "label": "",
+  const { user } = useContext(UserContext);
+  const [moodpointForm, setMoodpointForm] = useState({
+    energy: 0,
+    pleasantness: 0,
+    label: "",
   });
   const { isOpen, onOpen, onClose } = useDisclosure();
   const cancelRef = React.useRef();
   const style = styles();
-  const toast = useToast()
+  const toast = useToast();
 
   const handleGridSquareClick = (energy, pleasantness, label) => {
-    // TODO put real user id here
-    setMoodpointForm({energy: energy, pleasantness: pleasantness, label: label, user_id: 1})
+    setMoodpointForm({
+      energy: energy,
+      pleasantness: pleasantness,
+      label: label,
+      user_id: user?.user_id,
+    });
     onOpen();
   };
 
   const submitMoodpointForm = async () => {
-    console.log("submit the form", moodpointForm)
+    console.log("submit the form", moodpointForm);
     try {
       let response = await api.post("/moodpoints/", moodpointForm);
       setMoodpointForm({
@@ -48,11 +54,22 @@ function Grid() {
         pleasantness: 0,
         label: "",
       });
-      console.log("Here's the response: ", response)
-      toast({title: "The MoodPoint has been saved!", status: 'success', isClosable: true})
+      console.log("Here's the response: ", response);
+      toast({
+        title: "The MoodPoint has been saved!",
+        status: "success",
+        isClosable: true,
+      });
     } catch (error) {
-      console.log("There was an error saving this data point and here it is ", error)
-      toast({title: "Something has gone wrong, please try again later", status: 'error', isClosable: true})
+      console.log(
+        "There was an error saving this data point and here it is ",
+        error
+      );
+      toast({
+        title: "Something has gone wrong, please try again later",
+        status: "error",
+        isClosable: true,
+      });
     }
     onClose();
   };
@@ -61,35 +78,56 @@ function Grid() {
     <>
       <div className={style.squareContainer}>
         {GridData.map((data, index) => (
-          <div key={index}><GridSquare data={data} handleGridSquareClick={handleGridSquareClick}/></div>
+          <div key={index}>
+            <GridSquare
+              data={data}
+              handleGridSquareClick={handleGridSquareClick}
+            />
+          </div>
         ))}
       </div>
+
       <AlertDialog
-        motionPreset='slideInBottom'
+        motionPreset="slideInBottom"
         leastDestructiveRef={cancelRef}
         onClose={onClose}
         isOpen={isOpen}
         isCentered
       >
         <AlertDialogOverlay>
-          <AlertDialogContent>
-            <AlertDialogHeader fontSize="lg" fontWeight="bold">
-              Save MoodPoint
-            </AlertDialogHeader>
+          {user ? (
+            <AlertDialogContent>
+              <AlertDialogHeader fontSize="lg" fontWeight="bold">
+                Do you want to save this Mood Point?
+              </AlertDialogHeader>
 
-            <AlertDialogBody>
-              Do you want to save this MoodPoint?
-            </AlertDialogBody>
+              <AlertDialogFooter>
+                <Button ref={cancelRef} onClick={onClose}>
+                  NM
+                </Button>
 
-            <AlertDialogFooter>
-              <Button ref={cancelRef} onClick={onClose}>
-                Cancel
-              </Button>
-              <Button colorScheme="green" onClick={submitMoodpointForm} ml={3}>
-                Save
-              </Button>
-            </AlertDialogFooter>
-          </AlertDialogContent>
+                <Button
+                  colorScheme="green"
+                  onClick={submitMoodpointForm}
+                  ml={3}
+                >
+                  Yes!
+                </Button>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          ) : (
+            <AlertDialogContent>
+              <AlertDialogHeader fontSize="lg" fontWeight="bold">
+                You need to sign in or create an account to save this Mood Point.
+              </AlertDialogHeader>
+
+              <AlertDialogFooter>
+                <Button ref={cancelRef} onClick={onClose}>
+                  OK
+                </Button>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          )}
         </AlertDialogOverlay>
       </AlertDialog>
     </>
