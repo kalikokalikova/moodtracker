d4bb96d7ec17e9a2ac52eb130ada34ab49d58ac4
kalikokalikova <calico.seders@gmail.com>
2023-11-24 17:08:57 -0800

    graph basically working

--- client/src/Components/Graph/Graph.js
+++ client/src/Components/Graph/Graph.js
@@ -1,9 +1,14 @@
 import React, { useContext, useEffect, useState } from "react";
 import { UserContext } from "../../Hooks/UserContext";
 import api from "../../api";
+import { plugins } from "chart.js";
+import { Line } from "react-chartjs-2";
+import Chart from "chart.js/auto";
+import { CategoryScale } from "chart.js";
+
+Chart.register(CategoryScale);
 
 function Graph() {
-  console.log("foo");
   const { user } = useContext(UserContext);
   const [moodpoints, setMoodpoints] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -14,6 +19,7 @@ function Graph() {
       try {
         let response = await api.get(`/moodpoints/${user.user_id}`);
         console.log("Here's the response: ", response);
+        setMoodpoints(response.data)
       } catch (error) {
         console.log("Here's the error: ", error);
       }
@@ -22,9 +28,49 @@ function Graph() {
     fetchData();
   }, []);
 
+  const dates = moodpoints.map((mp) => mp.created_at)
+  const energyPoints = moodpoints.map((mp) => mp.energy)
+  const pleasantnessPoints = moodpoints.map((mp) => mp.pleasantness)
+
+  const graphData = {
+    labels: dates,
+    datasets: [
+      {
+        label: "Popularity of colors",
+        data: energyPoints,
+        backgroundColor: [
+          "rgba(255, 255, 255, 0.6)",
+          "rgba(255, 255, 255, 0.6)",
+          "rgba(255, 255, 255, 0.6)",
+        ],
+        borderWidth: 1,
+      },
+      {
+        label: "Pleasantness",
+        data: pleasantnessPoints,
+        backgroundColor: [
+            "",
+            "",
+            "",
+        ]
+      }
+    ],
+  };
+
   return (
     <>
       <div>Graph goes here</div>
+      <Line
+        data={graphData}
+        options={{
+          plugins: {
+            title: {
+              display: true,
+              text: "your feelings",
+            },
+          },
+        }}
+      />
     </>
   );
 }
